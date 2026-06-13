e1b4c5ebf7417ebdc2764f6a4ac63c94783b5b32
Cal0xim <blazhos@gmail.comgit config --global user.email blazhos@gmail.comgit config --global user.email blazhos@gmail.com>
2026-06-13 04:24:20 +0200

    webhook test

--- index.js
+++ index.js
@@ -81,4 +81,4 @@ client.once(Events.ClientReady, async (bot) => {
 // -------------------- LOGIN --------------------
 
 client.login(process.env.DISCORD_TOKEN);
-// No way LOLdsfdsfs
+// No way LOLdsfdsfshelp
