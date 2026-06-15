f3474fdd2819cb9b354089aaafc2e3e75bf6957e
Cal0xim <blazhos@gmail.comgit config --global user.email blazhos@gmail.comgit config --global user.email blazhos@gmail.com>
2026-06-16 00:47:56 +0200

    test webhook

--- components/leaderboard.js
+++ components/leaderboard.js
@@ -6,6 +6,9 @@ const {
     FileBuilder
 } = require('discord.js');
 
+const fs = require('fs');
+const path = require('path');
+
 const HIGHLIGHT_CREW = "Frostwhale Fumblers";
 
 const BOT_INFO_PATH = path.join(
